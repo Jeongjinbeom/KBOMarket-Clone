52c8f77f10fa9fa77f75788fb215649d7b9095d1
hyeon <kshyeon114@gmail.com>
2024-10-18 04:17:03 +0900

    cart, order

--- src/main/java/goods/dao/OrderDAO.java
+++ src/main/java/goods/dao/OrderDAO.java
@@ -1,8 +1,12 @@
 package goods.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import goods.bean.CartDTO;
+import goods.bean.GoodsDTO;
+import goods.bean.OrderDTO;
 import user.bean.UserDTO;
 
 @Mapper
@@ -11,5 +15,19 @@ public interface OrderDAO {
 	public void insertCart(CartDTO cartDTO);
 
 	public UserDTO getOrderUser(String userId);
+
+	public GoodsDTO getGoods(int prdNo);
+
+	public void insertOrderInfo(OrderDTO orderDTO);
+
+	public List<OrderDTO> getOrderDTO(String userId);
+
+	public List<CartDTO> getCartDTO(String userId);
+
+	public GoodsDTO getGoodsList(int prdNo);
+
+	public void orderDelete(String ordNo);
+
+	public void cartDelete(String cartNo);
 
 }
--- src/main/resources/sql/KBOOrder.sql
+++ src/main/resources/sql/KBOOrder.sql
@@ -6,6 +6,11 @@ CREATE TABLE KBOOrder (
 	prdNo INT NOT NULL, -- 상품번호
 	orderPrice INT NOT NULL, -- 상품가격 (추가)
 	orderSize VARCHAR(50), -- 상품사이즈
+	name VARCHAR(100), -- 아이디
+	tel VARCHAR(15), -- 휴대폰번호
+	zipcode VARCHAR(30), -- 우편번호
+	addr1 VARCHAR(255), -- 도로명주소
+	addr2 VARCHAR(255), -- 상세주소
 	qty INT NOT NULL CHECK (qty > 0), -- 상품개수
 	regDate DATETIME DEFAULT CURRENT_TIMESTAMP, -- 구매날짜
 	
